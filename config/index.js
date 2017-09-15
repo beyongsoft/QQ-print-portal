931fdf7d077096d0e51f02b509bce742901f7ce5
huangshan <imhs0824@163.com>
2017-09-15 17:58:31 +0800

    使用了json-server，在vue中增加mock数据的功能

--- config/index.js
+++ config/index.js
@@ -27,7 +27,15 @@ module.exports = {
             autoOpenBrowser: true,
             assetsSubDirectory: 'static',
             assetsPublicPath: '/',
-            proxyTable: {},
+            proxyTable: {
+                '/api/': {
+                    target: 'http://localhost:3000',
+                    changeOrigin: true,
+                    pathRewrite: {
+                        '^/api/': ''
+                    }
+                }
+            },
             // CSS Sourcemaps off by default because relative paths are "buggy"
             // with this option, according to the CSS-Loader README
             // (https://github.com/webpack/css-loader#sourcemaps)
@@ -36,8 +44,4 @@ module.exports = {
             cssSourceMap: false
         }
     }
-    // export default new Router({
-    //             mode: 'history',
-    //             base: '/ttms/', //添加的地方
-    //             ...
-    //         }
+
