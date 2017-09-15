b53bbec66f6168bf43d6719dd62d0b7a8026329c
huangshan <imhs0824@163.com>
2017-09-15 18:01:01 +0800

    增加了mock数据的模块

---
+++ mock/routes.js
@@ -0,0 +1,10 @@
+module.exports={
+    "/qrcode/getSku": "/getSku",//获取sku的接口
+    "/qrcode/generate":"/generate",//获取二维码
+    "/scanQRCode/getSnAndPidByPinterId:printerId":"/scanQRCode",//通过扫描二维码获取相关信息
+    "/bindPrinter/bind":"/bind",//进行绑定
+    "/bindPrinter/bindInfo":"/bindInfo",//list页面显示所有绑定过的列表
+    "/printerJob/submitPrintJob":"/submitPrintJob",//上传文件的接口
+    "/printerJob/getPrintJobConfig":"/getPrintJobConfig"//打印设置的接口
+}
+
